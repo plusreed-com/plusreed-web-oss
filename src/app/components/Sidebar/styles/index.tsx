7d3a1c07abf76a32434d7218f0873ad25bfc1937
Reed Davenport <reed@plusreed.com>
2019-03-05 13:39:28 -0500

    Fix padding

--- src/app/components/Sidebar/styles/index.tsx
+++ src/app/components/Sidebar/styles/index.tsx
@@ -5,12 +5,12 @@ export const Heading = css`
     padding-left: 30px;
     font-family: 'Roboto', sans-serif;
     font-size: 36px;
-    margin-top: 10px;
+    margin-top: 15px;
 `;
 
 export const Subheading = css`
     color: #fff;
     padding-left: 30px;
-    margin-top: -10px;
+    margin-top: -5px;
     font-family: 'Roboto', sans-serif;
 `;
